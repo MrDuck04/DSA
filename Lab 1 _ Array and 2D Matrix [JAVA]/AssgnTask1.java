7f9c3e110800c1339fea35d9c58ecd6334590530
Abror Mahir <abrmahir122@gmail.com>
2025-10-23 15:13:02 +0600

    Refactor AssgnTask1 class and implement rotation method

--- Lab 1 _ Array and 2D Matrix [JAVA]/AssgnTask1.java
+++ Lab 1 _ Array and 2D Matrix [JAVA]/AssgnTask1.java
@@ -1,7 +1,39 @@
-public class main {
-    public static void main(String[] args) {
-        int examWeek = 3;
-        String[][] matrix = {
+//Assignment Task 01: Row Rotation Policy
+class AssgnTask1{
+
+    //Complete this method so that it gives the Expected Output
+    //YOU ONLY HAVE TO SUBMIT THIS METHOD, NO OTHER DRIVER CODE
+    public static String[][] rotation( String[][] matrix ) {
+
+        String[] temp = matrix[matrix.length - 1];
+        for (int i = matrix.length - 1; i > 0; i--) {
+            matrix[i] = matrix[i - 1];
+        }
+        matrix[0] = temp;
+        return matrix;     
+    }
+
+    public static Integer rowRotation( Integer examWeek, String[][] matrix ){
+        rotation(rotation(matrix));
+        for (String[] row : matrix) {
+            for (String element : row) {
+                System.out.print("| " + element + " ");
+            }
+            System.out.println("|");
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j].equals("AA")) {
+                    return i+1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //DO NOT CHANGE ANY DRIVER CODE BELOW THIS LINE
+    public static void main(String[] args){
+        String[][] seatStatus = {
                 {"A" , "B"  , "C"  , "D"  , "E"},
                 {"F" , "G"  , "H"  , "I"  , "J"},
                 {"K" , "L"  , "M"  , "N"  , "O"},
@@ -9,25 +41,23 @@ public class main {
                 {"U" , "V"  , "W"  , "X"  , "Y"},
                 {"Z" , "AA" , "BB" , "CC" , "DD"}
         };
-        for(int j = 0; j < matrix[j].length; j++){//selecting the cloumn
-            int k = j - examWeek +1;
-            for(int i = 0; i < matrix.length; i++){ //going through all the elements of the column
-                
-                String temp    = matrix[i][j];
-                matrix[i][j] = matrix[k][j];
-                matrix[k][j]   = temp;
-                System.out.println(matrix[i][j] + " | ");
-            }
-            System.out.println();
-        }
+        System.out.println("Given Seat Status: ");
+        Arr.print2D(seatStatus);
         
-        for(int i = 0; i < matrix.length; i++){
-            for(int j =  0; j < matrix[i].length; j++){
-                if("AA"== matrix[i][j]){
-                    examWeek = j;
-                }
-            }
-        }
-        System.out.println(examWeek + "is the examweek");
+        System.out.println("\nExpected Output:");
+        System.out.println("| U  | V  | W  | X  | Y  | ");
+        System.out.println("| Z  | AA | BB | CC | DD | ");
+        System.out.println("| A  | B  | C  | D  | E  | ");
+        System.out.println("| F  | G  | H  | I  | J  | ");
+        System.out.println("| K  | L  | M  | N  | O  | ");
+        System.out.println("| P  | Q  | R  | S  | T  | ");
+        System.out.println("Your friend AA will be on row 2");
+        
+        System.out.print("\nYour Output:\n");
+        Integer examWeek = 3;
+        //This following should print modified seat status after rotation and return the current exam week
+        Integer rowNumber = rowRotation( examWeek, seatStatus );
+        //The following should print Your friend AA will be on row 2
+        System.out.println("Your friend AA will be on row "+rowNumber);
     }
 }
